1f1dd667da2bb422efa5b60e92e5405918bbee18
duonghoccodefullstack <143280316+duonghoccodefullstack@users.noreply.github.com>
2024-01-18 22:41:02 +0700

    Update cau1.cpp

--- cau1.cpp
+++ cau1.cpp
@@ -1,34 +1,28 @@
+#include <iostream>
+#include <string>
 
-bool isPrime (int n ) {
-	if (n < 2 ) {
-		return  false ;
-	}
-	for (int i = 2 ; i <= sqrt(n);i ++) {
-		if (n % i == 0 ) {
-			return false;
-		}
-	}
-	return true ;
-} 
-int main () {
-	queue <int > q ;
-	int n ; cin >> n;
+int Tinh_tonguoc (int n) {
 	
-	for (int i = 2 ; i <= n ; i ++  ) {
-		if ( i < 10 && isPrime(i)) {
-			q.push(i);
-		}
-	}
-	   while (!q.empty()){
-       for (int i = 1; i <= 9; i++){
-       	
-           int k = q.front()*10 + i;
-           if ( k <= n && isPrime(k)){
-           	
-               q.push(q.front()*10 + i);
-           }
-       }
-       cout << q.front() << " ";
-       q.pop();
-   }
+	int sum = 0;
+	
+	 for ( int i = 1 ; i <= n ; i ++) {
+	 		if(n %i == 0) {
+	 			sum += i;
+			  }
+	  }
+	return sum;
+	
+}
+int main () {
+	int t ; cin >> t;
+int n;
+	for (int i =0 ; i < t; i++) {
+		 	
+		 cin >> n;
+		 	cout << Tinh_tonguoc(n) << endl;
+	 }
+//	 int sum1 =Tinh_tonguoc(n); 
+//	cout << sum1  << endl;
+
+	return 0;
 }
